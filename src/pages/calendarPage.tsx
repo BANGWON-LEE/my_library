c0e0e522f1f0654d8e7286bb692491b1577156a1
BANGWON-LEE <qkddnjs31@gmail.com>
2023-05-31 17:09:33 +0900

    달력 표현하기 위한 컴포넌트

---
+++ src/pages/calendarPage.tsx
@@ -0,0 +1,13 @@
+import Calendar from '@/components/calendar/Calendar';
+
+// import { Main } from '@/templates/Main';
+
+const CalendarPage = () => {
+  return (
+    <div className="mx-auto h-full w-full bg-[#FFFFFF]">
+      <Calendar />
+    </div>
+  );
+};
+
+export default CalendarPage;
