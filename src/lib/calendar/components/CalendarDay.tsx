72df2c5faf2cd16623023c0c1507038adbce1a17
BANGWON-LEE <qkddnjs31@gmail.com>
2023-06-02 00:35:30 +0900

    선택한 월 기준으로 이전 달, 다음 달을 원하는 배열 구조로 나오게 코드 수정

--- src/lib/calendar/components/CalendarDay.tsx
+++ src/lib/calendar/components/CalendarDay.tsx
@@ -1,17 +1,66 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 interface CalendarDayType {
   allDates: object[];
 }
 
 const CalendarDay = (props: CalendarDayType) => {
-  const [arrDayState, setArrDayState] = useState<object[]>();
+  // const [arrDayState, setArrDayState] = useState<object[]>();
   const toggleDay = (): void => {
     // console.log('이전123', dates);
   };
 
+  // j = i * j
+  // 0 1 2 3 4 5 6
+  // 7 8 9 10 11 12 13
+
+  const divideDay = (data: any) => {
+    console.log('data', data);
+    console.log('lng', data.length / 7);
+    // const dayArray: string[][] = [];
+    const dayArray: any = [];
+    const weekArray: any = ['', '', '', '', '', '', ''];
+    console.log('weeklng', data?.length);
+    let i = 0;
+    for (let j = 1; j <= data?.length; j += 1) {
+      const [type, value] = data[j - 1];
+      console.log('jj', j);
+      if (type === '0') {
+        // console.log('0번 타입', typeof data[j][1]);
+        // console.log('0번', data[j][1]);
+        weekArray[0] = value;
+        console.log('0번라인', weekArray[0], value);
+      } else if (type === '1') {
+        // console.log('1번');
+        weekArray[1] = value;
+        console.log('1번라인', weekArray[1], value);
+      } else if (type === '2') {
+        weekArray[2] = value;
+        console.log('2번라인', value);
+      } else if (type === '3') {
+        weekArray[3] = value;
+        console.log('3번라인', value);
+      } else if (type === '4') {
+        weekArray[4] = value;
+        console.log('4번라인', value);
+      } else if (type === '5') {
+        weekArray[5] = value;
+        console.log('5번라인', value);
+      } else if (type === '6') {
+        weekArray[6] = value;
+        console.log('day', weekArray);
+        dayArray[i] = [...weekArray];
+        i += 1;
+        console.log('6번라인', value);
+      }
+    }
+    console.log('dayArray', dayArray);
+  };
+
   useEffect(() => {
-    setArrDayState(props.allDates);
+    if (props.allDates !== undefined) {
+      divideDay(props.allDates);
+    }
   }, [props.allDates]);
 
   console.log('allD2', arrDayState);
