5d9b11609b1e10358282c791690e84b0df5ebb53
BANGWON-LEE <qkddnjs31@gmail.com>
2023-06-04 19:12:35 +0900

    콘솔 제거 및 dom에 보여질 날짜들 범위 조절

--- src/lib/calendar/components/CalendarDay.tsx
+++ src/lib/calendar/components/CalendarDay.tsx
@@ -15,47 +15,33 @@ const CalendarDay = (props: CalendarDayType) => {
   // 7 8 9 10 11 12 13
 
   const divideDay = (data: any) => {
-    console.log('data', data);
-    console.log('lng', data.length / 7);
-    // const dayArray: string[][] = [];
     const dayArray: object[][] = [];
     const weekArray: any = ['', '', '', '', '', '', ''];
 
     let i = 0;
-    for (let j = 1; j <= data?.length; j += 1) {
-      const [type, value] = data[j - 1];
-      console.log('jj', j);
+    for (let j = 0; j < data?.length; j += 1) {
+      const [type, value] = data[j];
       if (type === '0') {
-        // console.log('0번 타입', typeof data[j][1]);
-        // console.log('0번', data[j][1]);
         weekArray[0] = value;
-        console.log('0번라인', weekArray[0], value);
       } else if (type === '1') {
         // console.log('1번');
         weekArray[1] = value;
-        console.log('1번라인', weekArray[1], value);
       } else if (type === '2') {
         weekArray[2] = value;
-        console.log('2번라인', value);
       } else if (type === '3') {
         weekArray[3] = value;
-        console.log('3번라인', value);
       } else if (type === '4') {
         weekArray[4] = value;
-        console.log('4번라인', value);
       } else if (type === '5') {
         weekArray[5] = value;
-        console.log('5번라인', value);
       } else if (type === '6') {
         weekArray[6] = value;
-        console.log('day', weekArray);
+
         dayArray[i] = [...weekArray];
         i += 1;
-        console.log('6번라인', value);
       }
     }
     setArrDayState(dayArray);
-    console.log('dayArray', arrDayState);
   };
 
   useEffect(() => {
@@ -64,13 +50,11 @@ const CalendarDay = (props: CalendarDayType) => {
     }
   }, [props.allDates]);
 
-  console.log('allD2', arrDayState);
-
   // 주간의 날짜에 따라 보여주는 날짜 기간 수정
 
   return (
     <div>
-      {arrDayState?.map((el) => (
+      {arrDayState?.slice(0, 6).map((el) => (
         <div key={el.toString()}>
           <button type="button" onClick={() => toggleDay()}>
             {el.toString().replaceAll(',', '')}
