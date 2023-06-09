b6688b2f7d2e39b96c8a59a4478dcb9bb7540e80
BANGWON-LEE <qkddnjs31@gmail.com>
2023-06-09 10:32:29 +0900

    슬라이더 라이브러리를 감싸는 div 제거

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -8,19 +8,17 @@ import slide3 from '../../asset/brand_slider_img3.jpeg';
 
 const Slide = () => {
   return (
-    <div className="mb-5">
-      <SliderLib>
-        <div className="slider slide active h-[33.5rem] w-full">
-          <Image src={slide1} alt="이미지1" className="h-full" />
-        </div>
-        <div className="slider slide prev h-[33.5rem] w-full">
-          <Image src={slide2} alt="이미지2" className="h-full " />
-        </div>
-        <div className="slider slide next h-[33.5rem] w-full">
-          <Image src={slide3} alt="이미지3" className="h-full " />
-        </div>
-      </SliderLib>
-    </div>
+    <SliderLib>
+      <div className="slider slide active h-[33.5rem] w-full">
+        <Image src={slide1} alt="이미지1" className="h-full" />
+      </div>
+      <div className="slider slide prev h-[33.5rem] w-full">
+        <Image src={slide2} alt="이미지2" className="h-full " />
+      </div>
+      <div className="slider slide next h-[33.5rem] w-full">
+        <Image src={slide3} alt="이미지3" className="h-full " />
+      </div>
+    </SliderLib>
   );
 };
 
